728d09fc92753fcd9928d2cd9e9bfebcf1bfaedb
LiuYinghao <1807703506@qq.com>
2022-08-15 15:11:03 +0800

    初始化页面

---
+++ pages/index/index.vue
@@ -0,0 +1,38 @@
+<template>
+	<view class="container">
+		<!-- #ifdef H5 -->
+		<view style="text-align: center;">hello uni-app</view>
+		<!-- #endif -->
+		
+		
+		<!-- #ifdef APP-PLUS -->
+		<view style="text-align: center;">今天八月十五日</view>
+		<!-- #endif -->
+		
+		<!-- #ifndef MP-WEIXIN -->
+		<image src="../../static/logo.png" mode=""></image>
+		<!-- #endif -->
+		
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				href: 'https://uniapp.dcloud.io/component/README?id=uniui'
+			}
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style lang="scss">
+	.container {
+		padding: 20px;
+		font-size: 14px;
+		line-height: 24px;
+	}
+</style>
